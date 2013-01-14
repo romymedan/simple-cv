aaa2ed22c1d3ba296d4dd7614c26bfff8b322042
Romy Medan <romy.medan@gmail.com>
2013-01-14 00:43:15 -0500

    cleanup

--- handyman/js/cufon-replace.js
+++
@@ -1,1 +0,0 @@
-Cufon.replace('.menu li a, h3, h4, .support, h1 a, .button, .button-2', { fontFamily: 'NewsGoth BT', hover:true });
--- handyman/js/script.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function() { 
-	$(".list-services .tooltips").easyTooltip();
-}); 
